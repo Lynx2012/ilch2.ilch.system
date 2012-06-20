0a458347d2690b7bc07b0165dabf18dcab3108ff
KoernerWS <contact@florian-koerner.eu>
2012-06-20 21:51:08 +0200

    Beispielkonfiguration

---
+++ module-config-example.php
@@ -0,0 +1,69 @@
+<?php
+
+return array(
+    'modulname' => array(
+    	'meta' => array(
+			'key1' => NULL,
+			'key2' => NULL,
+			'...'  => NULL,
+		),
+		'title' => NULL,
+		'description' => NULL,
+		'licenses' => array(
+			array(
+				'name' => NULL,
+				'url' => NULL,
+			),
+			array(
+				'name' => NULL,
+				'url' => NULL,
+			),
+		),
+		'authors' => array(
+			array(
+				'realname' => NULL,
+				'nickname' => NULL,
+				'role' => NULL,
+				'homepage' => NULL,
+				'email' => NULL
+			),
+			array(
+				'realname' => NULL,
+				'nickname' => NULL,
+				'role' => NULL,
+				'homepage' => NULL,
+				'email' => NULL
+			),
+		),
+		'version' => NULL,
+		'history' => array(
+			'1.0' => array(
+				'date' => NULL,
+				'changelog' => array(
+					NULL,
+					NULL,
+					NULL,
+				)
+			)
+		),
+		'links' => array(
+			array(
+				'title' => NULL,
+				'url' => NULL,
+			),
+			array(
+				'title' => NULL,
+				'url' => NULL,
+			),
+		),
+		'required' => array(
+			'module_test',
+			'module_test2',
+			'module_test3',
+		),
+		'extends' => array(
+			'theme_blubba',
+			'module_test4'
+		)
+    )
+);
